76378f8bcc304389aa58de47965baaec1e03ca85
katherinGriffi <88365422+katherinGriffi@users.noreply.github.com>
2022-09-25 23:39:14 -0300

    Update db-Oficina.sql

--- db-Oficina.sql
+++ db-Oficina.sql
@@ -126,46 +126,60 @@ select * from mecanico;
 -- MÃO DE OBRA
 create table maoObra(
 	idMaoObra int auto_increment primary key,
+    tipoServico varchar(45),
     ValorMaoObra float not null
     );
+    
+-- INSERINDO DADOS EM MÃO DE OBRA
+insert into maoObra(tipoServico,ValorMaoObra) VALUES
+			('suspenção',1200),
+            ('cambio',800),
+            ('injecao',500),
+            ('motor',1500);
+select * from maoObra;
 
 -- PEÇAS
 
 create table Pecas(
 	idPecas int auto_increment primary key,
+    descricao varchar(45),
     valorPecas float not null
 );
+-- INSERINDO DADOS EM PEÇAS
+insert into Pecas(descricao,valorPecas) VALUES
+			('kit suspensão ',705.5),
+            ('peneus ',320),
+			('oleo',68.5),
+            ('motor',2100),
+             ('turbina',500),
+             ('lampada',48.7);
+select * from Pecas;
 
--- Ordem de Serviço
+
+-- ORDEM DE SERVIÇO
 
 create table OS(
-	idOS int auto_increment not null,
+	idOS int primary key auto_increment not null,
     dataEmissao varchar(25),
     dataConclusao varchar(25),
     statusOS enum('aberto','encerrado','em andamento'),
     idMaoObraOS int,
     idPecasOS int,
+    idEquipeMecanicosOS int,
     constraint fk_os_mao foreign key (idMaoObraOS) references maoObra(idMaoObra),
-    constraint fk_os_pecas foreign key (idPecasOS) references Pecas(idPecas)
-        
-   )
+    constraint fk_os_pecas foreign key (idPecasOS) references Pecas(idPecas),
+    constraint fk_os_equipe foreign key (idEquipeMecanicosOS) references equipeMecanicos(idEquipeMecanicos)
+   );
    
-   
-   idmechanical int not null,
-    idcustomer int not null,
-    idparts int not null,
-    idservice int not null,
-    datein datetime,
-    price decimal(4,1),
-    status varchar(15),
-    datefin date,
-    constraint pk_orders primary key (idorders),
-    constraint fk_orders_customer foreign key (idcustomer) references customer(idcustomer),
-    constraint fk_orders_mechanical foreign key (idmechanical) references mechanical(idmechanical),
-	constraint fk_orders_parts foreign key (idparts) references parts(idparts),
-    constraint fk_orders_service foreign key (idservice) references service(idservice)
-);
-
+  create table OS_Equipe(
+	idOS_Equipe int primary key auto_increment not null,
+    idOS int,
+    idEquipe int,
+    idAvaliacao int, 
+    constraint fk_osEquipe_os foreign key (idOS) references os(idOS),
+    constraint fk_osEquipe_equipe foreign key (idEquipe) references equipeMecanicos(idEquipeMecanicos),
+    constraint fk_osEquipe_avaliacao foreign key (idAvaliacao) references avaliacao(idAvaliacao)
+   );
 
 -- QUERIES-
 -- Que veiculos pertencem aos clientes?
@@ -195,6 +209,23 @@ select  m.nome AS "Mecanico",e.nomeEquipe from mecanico m, equipeMecanicos e
 	where idMecanicosEquipe=idMecanico and nome='Rafael Domingues'
     ;
 
+-- indicar que tipo de serviço veio fazer cada cliente
+     select c.nome,v.placa,v.marca,a.tipoServico   from veiculo v, avaliacao a, clientes c
+      where idVeiculoAvaliacao=idVeiculo and idClienteVeiculo=idCliente;
+ -- indicar que mecanico esta trabalhando no carro do cliente Roberto
+   select c.nome,v.placa,v.marca,a.tipoServico,m.nome  as Mecanico  from veiculo v, avaliacao a, clientes c, mecanico m, equipeMecanicos e
+      where idVeiculoAvaliacao=idVeiculo and idClienteVeiculo=idCliente and idMecanicosEquipe=idMecanico and idEquipeMecAvaliacao=idEquipeMecanicos
+      and c.nome ='Roberto'
+      ;
+      
+      
+ -- Quanto é o valor de cada OS?
+select idOS, ValorMaoObra,valorPecas from OS o, maoObra m, Pecas p
+	where o.idMaoObraOS=m.idMaoObra and o.idPecasOS=p.idPecas
+	group by idOS;
+-- quais sao os valores por tipo de serviço?
 
-select * from veiculo;
-select * from clientes;
+select tipoServico, sum(ValorMaoObra) As Total_Mao_Obra, sum(ValorPecas) as Total_Peças from OS o inner join Pecas  p
+		ON o.idPecasOS=p.idPecas
+        inner join maoObra m on o.idMaoObraOS=m.idMaoObra
+        group by tipoServico;
